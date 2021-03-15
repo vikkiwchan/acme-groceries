4629e3d7f324eba35a52581c142b70ed1d22328d
Vikki Chan <vikkiwchan@gmail.com>
2021-03-15 15:46:35 -0500

    added middleware and combineReducers

--- src/store.js
+++ src/store.js
@@ -1,25 +1,39 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import logger from 'redux-logger';
 
-const initialState = {
-  groceries: [],
-  view: ''
+// ACTION TYPES
+const LOAD = 'LOAD';
+const UPDATE = 'UPDATE';
+const CREATE = 'CREATE';
+const SET_VIEW = 'SET_VIEW';
+
+// REDUCER SLICES
+const groceryReducer = (state = [], action) => {
+  if (action.type === LOAD) {
+    return action.groceries;
+  }
+  if (action.type === UPDATE) {
+    return state.map((grocery) =>
+      grocery.id === action.grocery.id ? action.grocery : grocery
+    );
+  }
+  if (action.type === CREATE) {
+    return state.concat(action.grocery);
+  } else return state;
 };
-const store = createStore((state = initialState, action)=> {
-  if(action.type === 'LOAD'){
-    state = {...state, groceries: action.groceries };
-  }
-  if(action.type === 'UPDATE'){
-    state = {...state, groceries: state.groceries.map(grocery => grocery.id === action.grocery.id ? action.grocery : grocery )};
-  }
-  if(action.type === 'CREATE'){
-    state = {...state, groceries: [...state.groceries, action.grocery ]}
-  }
-  if(action.type === 'SET_VIEW'){
-    state = {...state, view: action.view};
-  }
-  return state;
-});
+
+const viewReducer = (state = '', action) => {
+  if (action.type === SET_VIEW) {
+    return action.view;
+  } else return state;
+};
+
+const store = createStore(
+  combineReducers({
+    groceries: groceryReducer,
+    view: viewReducer,
+  }),
+  applyMiddleware(logger)
+);
 
 export default store;
-
-
